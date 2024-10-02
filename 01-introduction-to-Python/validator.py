824c9a2a7cda612e080fc273690c97eabfdf5e53
hhomaei <homaei@modares.ac.ir>
2024-10-02 11:15:24 +0330

    Update validator.py

--- 01-introduction-to-Python/validator.py
+++ 01-introduction-to-Python/validator.py
@@ -1,6 +1,6 @@
 import re
 
-'''
+"""
 Regular Expressions (Regex)
 
 The regex module in Pythos = re
@@ -13,8 +13,8 @@ The regex module in Pythos = re
 {n,m} --->   matches at least n and at most m of the preceding group
     - (Ha){3} will match the string 'HaHaHa', but it will not match 'HaHa',
     - (Ha){3,5} can match three, four, or five instances
-\d    --->   anay numeric digit from 0 to 9
-\D    --->   any character that is not a numeric digit from 0 to 9.
+\\d    --->   anay numeric digit from 0 to 9
+\\D    --->   any character that is not a numeric digit from 0 to 9.
 ^     --->   the match must occur at the beginning of the searched text
     - ^spam means the string must begin with spam
 $     --->   the string must end with this regex pattern.
@@ -22,10 +22,9 @@ $     --->   the string must end with this regex pattern.
 .     --->   any character except for a newline
 [abc] matches any character between the brackets (such as a, b, or c).
 [^abc] matches any character that isn’t between the brackets
-'''
+"""
 
 def validate_email(email):
     if len(email) >= 6:
-        return bool(re.match("^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email))
+        return bool(re.match("^.+@[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email))
     return False
-
